2f1afab32772baf0a3a9b7cc74dfa379917d50d1
swetha <pswetha060@gmail.com>
2025-06-05 16:57:42 +0530

    robo

--- roboshop.sh
+++ roboshop.sh
@@ -5,7 +5,7 @@ instances=("mongodb" "redis" "mysql" "catalogue" "rabbitmq" "user" "cart" "shiip
 zoneid="Z06739001OLF9ITWI9A5S"
 domain="swetha.fun"
 
-for instance in ${(instances[@])}
+for instance in ${instances[@]}
 do 
    instance_id= $(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro 
   --security-group-ids sg-0a678a7f0d29620a4  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=sample1}]' --query "instance[0].instanceid" --output text)
