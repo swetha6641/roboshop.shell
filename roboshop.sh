2ade986d55528359f1185d4b83a547301808b76e
swetha <pswetha060@gmail.com>
2025-06-13 12:11:10 +0530

    msgh

--- roboshop.sh
+++ roboshop.sh
@@ -7,7 +7,7 @@ domain="swetha.fun"
 ##jjjer
 for instance in ${INSTANCES[@]}
 do 
-   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0a678a7f0d29620a4  --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0a678a7f0d29620a4 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
   if [ $instance != "frontened" ]
   then 
      IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
