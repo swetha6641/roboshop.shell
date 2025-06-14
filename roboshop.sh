8c34e602235ff6c98aad9019fd6350d65156b16f
swetha <pswetha060@gmail.com>
2025-06-14 13:10:56 +0530

    msgh

--- roboshop.sh
+++ roboshop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0a678a7f0d29620a4" # replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
@@ -8,8 +9,7 @@ DOMAIN_NAME="swetha.fun" # replace with your domain
 #for instance in ${INSTANCES[@]}
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0a678a7f0d29620a4 
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0a678a7f0d29620a4 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
