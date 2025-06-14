bc18d41c6f8f1f537edf1c349b3e49134fb02aba
swetha <pswetha060@gmail.com>
2025-06-14 11:39:14 +0530

    msgh

--- roboshop.sh
+++ roboshop.sh
@@ -7,7 +7,7 @@ domain="swetha.fun"
 
 for instance in ${INSTANCES[@]}
 do 
-   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0a678a7f0d29620a4 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0a678a7f0d29620a4 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
   if [ $instance != "frontened" ]
   then 
      IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
@@ -15,5 +15,28 @@ do
      IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
    fi
    echo "$instance IP ADDRESS: $IP"
+
+   aws route53 change-resource-record-sets \
+    --hosted-zone-id $zoneid \
+    --change-batch '
+    {
+       "comment" : "creating or updating a record for cognito"
+        "Changes": [
+            {
+                "Action": "UPSERT",
+                "ResourceRecordSet": {
+                    "Name": "'$instance'.'$domain",
+                    "Type": "A",
+                    "TTL": 1,
+                    "ResourceRecords": [
+                        {
+                            "Value": "'$IP'"
+                        }
+                    ]
+                }
+            }
+        ]
+    }'
+
 done 
  
