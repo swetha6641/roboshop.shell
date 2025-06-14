69ea0c69c9a216b3334997e2ea0e9e8066e67ebe
swetha <pswetha060@gmail.com>
2025-06-14 12:58:01 +0530

    msgh

--- roboshop.sh
+++ roboshop.sh
@@ -20,23 +20,17 @@ do
     --hosted-zone-id $zoneid \
     --change-batch '
     {
-       "comment" : "creating or updating a record for cognito",
-        "Changes": [
-            {
-                "Action"  : "UPSERT",
-                "ResourceRecordSet"  : {
-                    "Name"  : "'$instance'.'$domain",
-                    "Type" : "A",
-                    "TTL" : 1,
-                    "ResourceRecords"  : [
-                        {
-                            "Value"  : "'$IP'"
-                        }
-                    ]
-                }
-            }
-        ]
+        "Comment": "Creating or Updating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$instance' . '$domain'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
     }'
-
-done 
- 
+done
