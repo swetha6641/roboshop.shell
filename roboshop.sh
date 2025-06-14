1c85899e94d8de92552874d0a2442eaecf88bd52
swetha <pswetha060@gmail.com>
2025-06-14 13:12:02 +0530

    msgh

--- roboshop.sh
+++ roboshop.sh
@@ -10,7 +10,7 @@ DOMAIN_NAME="swetha.fun" # replace with your domain
 for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0a678a7f0d29620a4 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
-    if [ $instance != "frontend" ]
+    if [ $instance != "frontend" ]hhhj
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
         RECORD_NAME="$instance.$DOMAIN_NAME"
